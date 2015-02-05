4e847e549c193cac77e47877762259141fccb6f7
lber19535 <lber19535@126.com>
2015-02-05 20:15:28 +0800

    [Bill Lv] 注册service

--- ZhiHu/src/com/bill/zhihu/login/ActivityLogin.java
+++ ZhiHu/src/com/bill/zhihu/login/ActivityLogin.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import com.bill.zhihu.R;
 import com.bill.zhihu.activity.BaseActivity;
 
+/**
+ * 登录界面
+ * 
+ * @author Bill Lv
+ *
+ */
 public class ActivityLogin extends BaseActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
 
 	}
 
