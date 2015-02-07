b47f930e7c4859deddd09615b48ac57e5ce6b786
lber19535 <lber19535@126.com>
2015-02-07 22:31:10 +0800

    [Bill Lv] 加入进程间通信

--- ZhiHu/src/com/bill/zhihu/login/FragmentLogin.java
+++ ZhiHu/src/com/bill/zhihu/login/FragmentLogin.java
@@ -1,9 +1,16 @@
 package com.bill.zhihu.login;
 
 import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +18,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.bill.zhihu.R;
+import com.bill.zhihu.api.binder.aidl.ILogin;
 
 /**
  * 登录
@@ -29,6 +37,21 @@ public class FragmentLogin extends Fragment {
 	private RelativeLayout loginLayout;
 
 	private View rootView;
+	private ILogin login;
+
+	private ServiceConnection connection = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			login = null;
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			login = ILogin.Stub.asInterface(service);
+			System.out.println("onServiceConnected");
+		}
+	};
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,6 +67,29 @@ public class FragmentLogin extends Fragment {
 		captchaEdt = (EditText) rootView.findViewById(R.id.login_captcha);
 		loginBtn = (Button) rootView.findViewById(R.id.login_btn);
 		loginLayout = (RelativeLayout) rootView.findViewById(R.id.login_layout);
+
+		Intent intent = new Intent();
+		intent.setClassName("com.bill.zhihu.api",
+				"com.bill.zhihu.api.service.LoginService");
+		intent.setAction("com.bill.zhihu.api.service.login");
+		getActivity().bindService(intent, connection, Context.BIND_AUTO_CREATE);
+
+		loginBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				try {
+					int code = login.login("hello", "world", "!");
+					System.out.println(code);
+					System.out.println(login.haveCaptcha());
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
 	}
 
 }
--- ZhiHuApi/src/com/bill/zhihu/api/service/LoginService.java
+++ ZhiHuApi/src/com/bill/zhihu/api/service/LoginService.java
@@ -1,24 +1,49 @@
 package com.bill.zhihu.api.service;
 
+import com.bill.zhihu.api.binder.aidl.ILogin;
+
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.IBinder;
+import android.os.RemoteException;
 
 /**
  * 登录服务 1.登录 2.登出 3.验证码
  *
- * 验证码可以通过aidl传递（Bitmap继承了parcelable接口可以直接传递），
- * 也可以存在本地传递地址，实现的时候分别验证下效率
+ * 验证码可以通过aidl传递（Bitmap继承了parcelable接口可以直接传递）， 也可以存在本地传递地址，实现的时候分别验证下效率
  * 
  * @author Bill Lv
  *
  */
 public class LoginService extends Service {
 
+	private ILogin.Stub mBinder;
+
+	public LoginService() {
+		System.out.println("create service");
+	}
+
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+		System.out.println("on bind");
+		mBinder = new ILogin.Stub() {
+
+			@Override
+			public int login(String account, String pwd, String captcha)
+					throws RemoteException {
+				System.out.println("service " + account + " " + pwd + " "
+						+ captcha);
+				return 2333;
+			}
+
+			@Override
+			public boolean haveCaptcha() throws RemoteException {
+				return true;
+			}
+		};
+
+		return mBinder;
 	}
 
 }
