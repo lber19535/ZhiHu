57d40c7c7a193b0990b03e72a168f19c1929d787
lber19535 <lber19535@126.com>
2015-02-15 10:52:30 +0800

    [Bill Lv] 清除cookie 清除缓存 获取已用缓存大小， 清除cookies的意义在于可以免验证码登录

--- ZhiHu/src/com/bill/zhihu/api/ZhihuApi.java
+++ ZhiHu/src/com/bill/zhihu/api/ZhihuApi.java
@@ -1,13 +1,42 @@
 package com.bill.zhihu.api;
 
+import java.io.File;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.bill.zhihu.ZhihuApp;
 import com.bill.zhihu.api.cmd.Command;
+import com.bill.zhihu.api.utils.ZhihuCookieStore;
 
 public class ZhihuApi {
-	
+
 	public static final String XSRF = "_xsrf";
+	public static Context mContext = ZhihuApp.getContext();
 
 	public static void execCmd(Command cmd) {
 		cmd.exec();
+	}
+
+	public static void clearCookies() {
+		new ZhihuCookieStore(mContext).clear();
+	}
+
+	public static void clearCache() {
+		File cacheDir = mContext.getCacheDir();
+		File[] cacheFiles = cacheDir.listFiles();
+		for (File file : cacheFiles) {
+			if (!file.delete()) {
+				Toast.makeText(mContext, "有些缓存文件无法删除", Toast.LENGTH_SHORT)
+						.show();
+			}
+		}
+	}
+
+	public static float getCacheUsableSpace() {
+		File cacheDir = mContext.getCacheDir();
+		long usableSpace = cacheDir.getUsableSpace();
+		return usableSpace / 1024f;
 	}
 
 }
