3b35989db642ec247303c9048e37826002413747
Bill_lv <lber19535@126.com>
2016-05-30 14:41:15 +0800

    [Bill Lv] test ci deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PROJECT_PATH="$(pwd)"
+APK_PATH="$PROJECT_PATH/app/build/outputs/apk/"
+
+echo "$APK_PATH"
+
+RELEASE_APK_NAME="$(find $APK_PATH -name '*release.apk')"
+echo "$RELEASE_APK_NAME"
+
+mv "$RELEASE_APK_NAME" "$APK_PATH/zhihu.apk"
