9818dc5040aefeb79909f8c6caa74fcd0def22a4
Bill_lv <lber19535@126.com>
2016-05-30 16:10:19 +0800

    [Bill Lv] test ci deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ DATE="$(date +'%Y-%m-%d')"
 echo "$APK_PATH"
 
 RELEASE_APK_NAME="$(find $APK_PATH -name '*release.apk')"
+
 echo "$RELEASE_APK_NAME"
 
-mv "$RELEASE_APK_NAME" "$APK_PATH/zhihu-$TRAVIS-TAG-$DATE-release.apk"
+mv "$RELEASE_APK_NAME" "$APK_PATH/zhihu.apk"
+
